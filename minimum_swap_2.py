334820d85fb5ef064ef626e4537867939930bd2d
Arun Kumar <aanand01762@gmail.com>
2020-03-19 23:57:55 +0530

    Add description or more information about the flow of code

--- minimum_swap_2.py
+++ minimum_swap_2.py
@@ -2,17 +2,31 @@
 
 def minimumSwaps(arr):
     swap = 0
-    index = [0]*len(arr)
+    indexs = [0]*len(arr)
+
+    # Iterate index and value together
+    # store index of the value at the index which which is value
     for i, value in enumerate(arr):
-        index[value-1] = i
-    
+        indexs[value-1] = i
+
     for i in range(len(arr)):
+
+        # Check if the value is not at right index
         if arr[i] != i+1:
-            Index = index[i]
-            arr[Index] = arr[i]
+
+            # Get the right index of value
+            index_value = indexs[i]
+
+            # Swap the right value which is at Index and value at i
+            arr[index_value] = arr[i]
             arr[i] = i+1
 
-            index[arr[Index]-1] = Index
-            index[i] = i
+            # Now index of pervious arr[i] is index_value
+            # so update it in indexs list
+            indexs[arr[index_value]-1] = index_value
+
+            # Now arr[i] or i+1 is at right index so update indexs list also
+            indexs[i] = i
+
             swap += 1
     return swap
